2a97917863057c374954ddbf98c57a5bcd82f154
Lyashka <marzlyaha@mail.ru>
2023-09-20 22:38:09 +0300

    v1.0

--- index.js
+++ index.js
@@ -1,14 +1,29 @@
 const btns = document.querySelectorAll("button");
 btns.forEach(function (btn) {
-  btn.addEventListener("click", function () {
-    // console.log("Button clicked " + e.target.textContent);
+  btn.addEventListener("click", function (e) {
+    const btnImage = btn.querySelector(".img_plus");
+
+    if (btn.classList.contains("opened_btn") == false) {
+      btn.classList.add("opened_btn");
+      btnImage.src = "./assets/img_btn_minus.svg";
+    } else {
+      btn.classList.remove("opened_btn");
+      btnImage.src = "./assets/img_btn_plus.svg";
+    }
+
     const targetId = this.getAttribute("target_landmark");
-    console.log(targetId);
     const infoHouse = document.getElementById(targetId);
+
     if (infoHouse.style.display === "none" || infoHouse.style.display === "") {
       infoHouse.style.display = "block";
     } else {
       infoHouse.style.display = "none";
+    }
+
+    if (btn.classList.contains("style_btn_blue") == true) {
+      infoHouse.classList.add("color_label_blue");
+    } else {
+      infoHouse.classList.add("color_label_green");
     }
   });
 });
